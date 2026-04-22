63b8f492e3ec64c19006248ef3907e363297f5a1
AhmedElabrass25 <ahmedelabrass25@gmail.com>
2026-04-22 13:21:16 +0200

    feat:edit in api.ts file make export

--- client/src/lib/Api.ts
+++ client/src/lib/Api.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
-
-const api = axios.create({
+export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL + "/api" || "http://localhost:5000",
   headers: {
     "Content-Type": "application/json",
